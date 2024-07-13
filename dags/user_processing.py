af87b7e1bc0925051ea50f9bda9b4f7ecb18d694
Pratyush Vishal <pratyushvishal04@gmail.com>
2024-07-14 00:05:22 +0530

    Add extract user

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -3,6 +3,8 @@ from airflow import DAG
 from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.http.operators.http import SimpleHttpOperator
+import json
 
 with DAG('user_processing', start_date = datetime(2024,7,11),
             schedule_interval='@daily', catchup=False) as dag: 
@@ -19,14 +21,23 @@ with DAG('user_processing', start_date = datetime(2024,7,11),
             password text not null,
             email text not null
             );
-'''
+        '''
     )
 
 
-is_api_available = HttpSensor(
-    task_id= 'is_api_available',
-    http_conn_id = 'user_api',
-    endpoint="api/"
-)
+    is_api_available = HttpSensor(
+        task_id= 'is_api_available',
+        http_conn_id = 'user_api',
+        endpoint="api/"
+    )
+
+    extract_user = SimpleHttpOperator(
+        task_id = 'extract_user',
+        http_conn_id = 'api/',
+        method = 'GET',
+        response_filter = lambda response:json.loads(response.text),
+        log_response = True
+    )
+
 
 
