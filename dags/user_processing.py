899a8e1467f0939fde78fea8e640cb10f092dc74
Pratyush Vishal <pratyushvishal04@gmail.com>
2024-07-14 00:57:05 +0530

    created dependencies and DAG Pipeline

--- dags/user_processing.py
+++ dags/user_processing.py
@@ -4,7 +4,32 @@ from datetime import datetime
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.providers.http.sensors.http import HttpSensor
 from airflow.providers.http.operators.http import SimpleHttpOperator
+from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.hooks.postgres import PostgresHooks
 import json
+from pandas import json_normalize
+
+
+#to pull data that is loaded by task extract_user
+def _process_user(ti): #ti->task instance
+    user = ti.xcom_pull(task_ids = "extract_user")
+    user = user['results'][0]
+    processed_user = json_normalize({
+        'firstname': user['name']['first'],
+        'lastname': user['name']['last'],
+        'country': user['location']['country'],
+        'username': user['login']['username'],
+        'password': user['login']['password'],
+        'email':user['email']
+    })
+    processed_user.to_csv('/tmp/processed_user.csv', index=None, header=False)
+
+def _store_user():
+    hook = PostgresHooks(postgres_conn_id = 'postgres')
+    hook.copy_expert(
+        sql = "COPY users FROM stdin WITH DELIMETER as ','",
+        filename = '/tmp/processed_user.csv'
+    )
 
 with DAG('user_processing', start_date = datetime(2024,7,11),
             schedule_interval='@daily', catchup=False) as dag: 
@@ -39,6 +64,19 @@ with DAG('user_processing', start_date = datetime(2024,7,11),
         response_filter = lambda response:json.loads(response.text),
         log_response = True
     )
+
+    process_user = PythonOperator(
+        task_id = 'process_user',
+        python_callable = _process_user
+    )
+
+    store_user = PythonOperator(
+        task_id = 'store_user',
+        python_callable = _store_user
+    )
+
+    #Dependencies 
+    create_table >> is_api_available >> extract_user >> process_user >> store_user
 
 
 
